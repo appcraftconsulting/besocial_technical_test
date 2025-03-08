e3788f31345215e258444981ad99c90e9f392f19
François Boulais <contact@appcraftconsulting.com>
2025-03-08 20:42:15 +0100

    Initial Commit

---
+++ SocialStories/SocialStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialStoriesApp.swift
+//  SocialStories
+//
+//  Created by François Boulais on 08/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SocialStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
